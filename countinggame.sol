c89900dcc1a7330bae4c13ceb15fc48064c5a860
Elliot <112030301+BraverElliot@users.noreply.github.com>
2022-12-14 20:43:29 -0800

    Create countinggame.sol

---
+++ countinggame.sol
@@ -0,0 +1,20 @@
+/**
+ * SPDX-License-Identifier:MIT
+*/
+
+// made by BraverElliot.eth (send me money)
+pragma solidity ^0.8.7;
+
+contract CaptureTheFlag {
+
+
+    address [] public userArray;
+    uint256 public number;
+    constructor(){
+        number=0;
+    }
+    function count() public {
+        userArray.push(msg.sender);
+        number+=1;
+    }
+}
