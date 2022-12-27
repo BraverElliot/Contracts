d7375b2614e9ccdbefbd55c06710043906968bfb
Elliot <112030301+BraverElliot@users.noreply.github.com>
2022-12-26 21:44:05 -0800

    Update Voting_Contract.sol

--- Voting_Contract.sol
+++ Voting_Contract.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.8.7;
+
 // SPDX-License-Identifier: MIT
 
 /**
@@ -6,7 +7,6 @@ pragma solidity ^0.8.7;
  * @author Elliot
  */
 contract Voting {
-    address public owner;
 
     uint256 public OptionACount = 0;
     uint256 public OptionBCount = 0;
@@ -16,35 +16,35 @@ contract Voting {
 
     mapping(address => bool) public hasVoted;
 
-    constructor() public {
-        owner = msg.sender;
-    }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can perform this action");
-        _;
-    }
 
-    function openVoting() public onlyOwner {
+    function openVoting () public {
         isOpen = true;
     }
 
-    function closeVoting() public onlyOwner {
+    function closeVoting () public {
         isOpen = false;
     }
 
-    function voteOptionA() public {
+    function voteOptionA () public {
         require(hasVoted[msg.sender] != true, "user has already voted");
         require(isOpen, "voting is closed");
-        OptionACount += 1;
-        TotalVotes += 1;
+
+
+        hasVoted[msg.sender] = false;
+
+        OptionACount+=1;
+        TotalVotes+=1;
     }
-
-    function voteOptionB() public {
+    function voteOptionB () public {
         require(hasVoted[msg.sender] != true, "user has already voted");
         require(isOpen, "voting is closed");
 
-        OptionBCount += 1;
-        TotalVotes += 1;
+
+        hasVoted[msg.sender] = false;
+
+
+        OptionBCount+=1;
+        TotalVotes+=1;
     }
 }
